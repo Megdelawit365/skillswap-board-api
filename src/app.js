1c82f081b81ec65e6d9717a5b7b9968d8d13e481
Megdelawit365 <megdelawitaynalem365@gmail.com>
2026-08-25 18:01:40 +0300

    added request logger middleware

--- src/app.js
+++ src/app.js
@@ -1,6 +1,8 @@
 import express from "express"
+import requestLogger from "./middleware/requestLogger"
 
 const app = express()
 app.use(express.json())
 
+app.use(requestLogger)
 export default app
---
+++ src/middleware/requestLogger.js
@@ -0,0 +1,4 @@
+export default requestLogger = (req, res, next) => {
+    console.log(req.method, req.path)
+    next()
+}
