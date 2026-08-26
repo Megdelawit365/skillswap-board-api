6284a0ba383b3c91d4ab669a41c59cba63f1c240
Megdelawit365 <megdelawitaynalem365@gmail.com>
2026-08-26 20:24:04 +0300

    added routes for listingsand responses

--- src/app.js
+++ src/app.js
@@ -1,8 +1,13 @@
 import express from "express"
-import requestLogger from "./middleware/requestLogger"
+import requestLogger from "./middleware/requestLogger.js"
+import listingRoutes from "./routes/listingRoutes.js"
+import responseRoutes from "./routes/responseRoutes.js"
 
 const app = express()
 app.use(express.json())
-
 app.use(requestLogger)
+
+app.use("/api/listings", listingRoutes)
+app.use("/api/listings/:id/responses", responseRoutes)
+
 export default app
