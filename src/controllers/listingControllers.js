fb76c80ca4d646de15e642fc6cb5dfbb24bc94c9
Megdelawit365 <megdelawitaynalem365@gmail.com>
2026-08-26 17:51:16 +0300

    added controllers for listings crud

--- src/controllers/listingControllers.js
+++ src/controllers/listingControllers.js
@@ -23,3 +23,43 @@ export const getListingById = async (req, res) => {
         res.status(500).json({ error: "Internal server error." })
     }
 }
+export const createListing = async (req, res) => {
+    try {
+        const body = req.body
+        const newListing = await listingService.createListing(body)
+        return res.status(201).json({
+            message: "Listing created successfully.",
+            data: newListing
+        })
+    } catch (err) {
+        console.log(err)
+        res.status(500).json({ error: "Internal server error." })
+    }
+}
+export const updateListing = async (req, res) => {
+    try {
+        const id = req.params.id
+        const body = req.body
+        const updatedMovie = await listingService.updateListing(id, body)
+        return res.status(200).json({
+            message: "Listing updated successfully.",
+            data: updatedMovie
+        })
+    } catch (err) {
+        console.log(err)
+        res.status(500).json({ error: "Internal server error." })
+    }
+}
+export const deleteListing = async (req, res) => {
+    try {
+        const id = req.params.id
+        const listing = await listingService.deleteListing(id)
+        if (!listing) {
+            return res.status(404).json({ message: "Listing not found." })
+        }
+        return res.status(204).send()
+    } catch (err) {
+        console.log(err)
+        res.status(500).json({ error: "Internal server error." })
+    }
+}
