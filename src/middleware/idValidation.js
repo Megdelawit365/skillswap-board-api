a06b81e5dd8acbeb17257971503ad0741733222b
Megdelawit365 <megdelawitaynalem365@gmail.com>
2026-08-26 20:48:50 +0300

    added middleware for validating id

---
+++ src/middleware/idValidation.js
@@ -0,0 +1,9 @@
+export default validateId = (req, res, next) => {
+    const id = Number(req.params.id)
+    if (isNaN(id) || typeof (id) != "number" || id <= 0) {
+        return res.status(400).json({
+            message: "Invalid Id."
+        })
+    }
+    next()
+}
