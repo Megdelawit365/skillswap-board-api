7d57d14834db7582ab2cdfec9205ecf7cccc5205
Megdelawit365 <megdelawitaynalem365@gmail.com>
2026-08-26 20:06:21 +0300

    added controllers for responses

---
+++ src/controllers/responseControllers.js
@@ -0,0 +1,27 @@
+import * as responseService from "../services/responseServices.js"
+
+export const getResponses = async (req, res) => {
+    try {
+        const responses = await responseService.getResponses(req.params.id)
+        return res.status(200).json({
+            data: responses
+        })
+    } catch (err) {
+        console.log(err)
+        res.status(500).json({ error: "Internal server error." })
+    }
+}
+export const createResponses = async (req, res) => {
+    try {
+        const body = req.body
+        const id = req.params.id
+        const newResponse = await responseService.createResponse(id, body)
+        return res.status(201).json({
+            message: "Response added successfully.",
+            data: newResponse
+        })
+    } catch (err) {
+        console.log(err)
+        res.status(500).json({ error: "Internal server error." })
+    }
+}
