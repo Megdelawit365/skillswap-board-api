c36451736d20a2cead87f1b305237919396956d9
Megdelawit365 <megdelawitaynalem365@gmail.com>
2026-08-27 16:51:14 +0300

    connected listingId to id instead of just passing the id

--- src/services/responseServices.js
+++ src/services/responseServices.js
@@ -1,18 +1,20 @@
 import { prisma } from '../lib/prisma.js'
 
 export const getResponses = async (id) => {
-    const responses = prisma.response.findMany({
-        where: { listingId: id }
+    const responses = await prisma.response.findMany({
+        where: { listingId: Number(id) }
     })
     return responses
 }
 export const createResponse = async (id, body) => {
-    const { content, listingId } = body
-    const currentListing = prisma.listing.findUnique({ where: { id: id } })
-    const newResponse = await currentListing.create({
+    const { content } = body
+    const currentListing = await prisma.listing.findUnique({ where: { id: Number(id) } })
+    const newResponse = await prisma.response.create({
         data: {
             content: content,
-            listingId: listingId
+            listing: {
+                connect: { id: Number(id) }
+            }
         }
     })
     return newResponse
