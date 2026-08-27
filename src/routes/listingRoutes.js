79948528a9e1226dbfc477e956baab148d02e820
Megdelawit365 <megdelawitaynalem365@gmail.com>
2026-08-27 12:12:47 +0300

    added middlewares to routes

--- src/routes/listingRoutes.js
+++ src/routes/listingRoutes.js
@@ -1,12 +1,13 @@
 import { Router } from "express";
 import { createListing, deleteListing, getListingById, getListings, updateListing } from "../controllers/listingControllers.js"
 import { validateListing } from "../middleware/createValidation.js";
+import { validateId } from "../middleware/idValidation.js"
 const router = Router()
 
 router.get("/", getListings)
-router.get("/:id", getListingById)
+router.get("/:id", validateId, getListingById)
 router.post("/", validateListing, createListing)
-router.put("/:id", updateListing)
-router.delete("/:id", deleteListing)
+router.put("/:id", validateId, updateListing)
+router.delete("/:id", validateId, deleteListing)
 
 export default router
