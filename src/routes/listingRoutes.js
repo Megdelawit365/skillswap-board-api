4ef15e799e2140a5c536880212ab9309941a14a4
Megdelawit365 <megdelawitaynalem365@gmail.com>
2026-08-29 12:20:59 +0300

    added update validation middleware to listing update route

--- src/routes/listingRoutes.js
+++ src/routes/listingRoutes.js
@@ -2,12 +2,13 @@ import { Router } from "express";
 import { createListing, deleteListing, getListingById, getListings, updateListing } from "../controllers/listingControllers.js"
 import { validateListing } from "../middleware/createValidation.js";
 import { validateId } from "../middleware/idValidation.js"
+import { validateListingUpdate } from "../middleware/updateValidation.js";
 const router = Router()
 
 router.get("/", getListings)
 router.get("/:id", validateId, getListingById)
 router.post("/", validateListing, createListing)
-router.put("/:id", validateId, updateListing)
+router.put("/:id", validateId, validateListingUpdate, updateListing)
 router.delete("/:id", validateId, deleteListing)
 
 export default router
