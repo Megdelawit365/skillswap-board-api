27dfa24adc44505eda0853938cb3918360a77f84
Megdelawit365 <megdelawitaynalem365@gmail.com>
2026-08-29 12:19:18 +0300

    added more validations

--- src/middleware/updateValidation.js
+++ src/middleware/updateValidation.js
@@ -1,7 +1,14 @@
 export const validateListingUpdate = (req, res, next) => {
     const body = req.body
     const allowedFields = ["type", "skills"]
-    for (const [k, v] in Object.entries(body)) {
+
+    if (!body || Object.keys(body).length === 0) {
+        return res.status(400).json({
+            message: "No editable fields found."
+        })
+    }
+
+    for (const [k, v] of Object.entries(body)) {
         if (!allowedFields.includes(k)) {
             return res.status(400).json({
                 message: `${k} is not an editable field.`
@@ -9,19 +16,28 @@ export const validateListingUpdate = (req, res, next) => {
         }
     }
 
-    if (body.type) {
-        if (typeof (type) != "string" || type.trim() == "" || !["offering,wanting"].includes(type.toLowerCase())) {
+    if (body.type != undefined) {
+        if (typeof (body.type) != "string" || body.type.trim() == "" || !["offering", "wanting"].includes(body.type.trim().toLowerCase())) {
             return res.status(400).json({
                 message: "Type is missing or invalid."
             })
         }
     }
 
-    if (body.skills) {
-        if (skills == []) {
+    if (body.skills != undefined) {
+        if (!Array.isArray(body.skills) || body.skills.length === 0) {
             return res.status(400).json({
                 message: "Skills cannot be empty."
             })
         }
+        for (const skillId of body.skills) {
+            if (typeof skillId !== "number" || isNaN(skillId)) {
+                return res.status(400).json({
+                    message: "Skills array must contain numeric IDs."
+                });
+            }
+        }
     }
+
+    next()
 }
