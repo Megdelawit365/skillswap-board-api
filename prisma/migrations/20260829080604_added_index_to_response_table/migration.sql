93372d6e8c547ddeaa53153fee518f2d7541d107
Megdelawit365 <megdelawitaynalem365@gmail.com>
2026-08-29 11:07:35 +0300

    renamed migrations and added index

---
+++ prisma/migrations/20260829080604_added_index_to_response_table/migration.sql
@@ -0,0 +1,11 @@
+/*
+  Warnings:
+
+  - A unique constraint covering the columns `[name]` on the table `Skill` will be added. If there are existing duplicate values, this will fail.
+
+*/
+-- CreateIndex
+CREATE INDEX "Response_listingId_idx" ON "Response"("listingId");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Skill_name_key" ON "Skill"("name");
